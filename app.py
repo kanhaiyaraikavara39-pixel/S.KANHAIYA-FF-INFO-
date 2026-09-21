35c3a0225bd5d797ed69444216490c6a9ba5b238
kanhaiyaraikavara39-pixel <kanhaiyaraikavara39@gmail.com>
2026-09-21 19:53:11 +0530

    Update account credentials for region 'IND'

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ async def json_to_proto(json_data: str, proto_message: Message) -> bytes:
 def get_account_credentials(region: str) -> str:
     r = region.upper()
     if r == "IND":
-        return "uid=4973425281&password=KANHAIYA_KANHAIYA_FF_EXPERIMENT_ANeSTBSI"
+        return "uid=7868293003&password=S_KANHAIYA_mdlWrCkWD8aY"
     elif r in {"BR", "US", "SAC", "NA"}:
         return "uid=4558407324&password=SEXTY_MODS_IND_SAZJALSQB"
     else:
